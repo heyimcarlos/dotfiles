c828a94c7c583b950d4415cf17f251756ec404bc
Carlos De la Cruz <carlosdlc.dev@gmail.com>
2025-04-29 12:56:38 -0400

    add i3, picom, polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,9 +6,8 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
+# Launch bar1 
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar cj 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
